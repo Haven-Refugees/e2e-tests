c4d057e0a4117e51d3e67e313fd6c02c23ab4fb2
Oleksa <ale.jkav@gmail.com>
2023-07-21 01:20:36 -0600

    Update env map

--- cypress/plugins/index.ts
+++ cypress/plugins/index.ts
@@ -14,6 +14,34 @@ const buildConfiguration = (on: Cypress.PluginConfig, config: Cypress.PluginConf
 
 module.exports = buildConfiguration;
 
+const envMapKeys = {
+  accounts: [
+    'SUPPORTER_LOGIN',
+    'SUPPORTER_PASSWORD',
+    'REFUGEE_LOGIN',
+    'REFUGEE_PASSWORD',
+  ]
+}
+
+const hasValue = (value: any) => value !== null || typeof value !== 'undefined';
+
+const mutateEnvMapIfExistsToCypressEnv = (envMap) => {
+  envMap.accounts = {};
+  envMapKeys.accounts.forEach((key) => {
+    if (hasValue(process.env[key])) {
+      const [accountType, fieldName] = (key).toLowerCase().split('_');
+      envMap.accounts = {
+        ...envMap.accounts,
+        [accountType]: {
+          ...envMap.accounts[accountType],
+          [fieldName]: process.env[key]
+        }
+      }
+    }
+  })
+}
+
+
 // This code is executed before the configs are loaded.
 module.exports = (on: Cypress.PluginConfig, config: Cypress.PluginConfigOptions) => {
   const finalConfig = config;
@@ -26,5 +54,6 @@ module.exports = (on: Cypress.PluginConfig, config: Cypress.PluginConfigOptions)
     ...finalConfig.env,
     ...buildConfiguration(on, config)
   }
+  mutateEnvMapIfExistsToCypressEnv(finalConfig.env);
   return finalConfig;
 };
